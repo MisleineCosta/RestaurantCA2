ace0a5f2a3b22c81f410b92931dd30d41d6ca8dc
MisleineCosta <costaleine@icloud.com>
2021-04-29 11:38:03 +0000

    install package

---
+++ app.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+app.use(express.json())
+// const productRouters = require('./api/routers/products');
+// const orderRouters = require('./api/routers/orders');
+
+app.listen(6000, () => console.log('Server Started'))
