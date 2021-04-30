ac8a5a0e227663d8c199a69a636da671c2e94c82
MisleineCosta <costaleine@icloud.com>
2021-04-30 07:29:40 +0000

    edit router

--- routes.js
+++ routes.js
@@ -11,5 +11,17 @@ router.get('/prices/:id', priceCtrl.getPrice); //FIND USER BY id
 router.put('/prices/:id', priceCtrl.updatePrice); //UPDATES USER BY id
 router.delete('/prices/:id', priceCtrl.deletePrice); //DELETE USER BY id
 
+
+var multer = require("multer");
+
+var multer = require("multer");
+var upload = multer({ dest: module.exports.UPLOAD_PATH});
+var imageCtrl = require('./price-controller');
+
+router.post('/prices', upload.single('price'), priceCtrl.uploadPrice);
+router.get('/prices', imageCtrl.getPrices);
+router.get('/prices/:id', imageCtrl.getPrice);
+router.delete('/prices/:id', priceCtrl.deletePrice);
+
 //EXPORTS
 module.exports = router;
