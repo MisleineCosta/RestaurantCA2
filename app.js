e7fe879f53698aa6f3e92580fd4b68a5a4e0b076
MisleineCosta <costaleine@icloud.com>
2021-05-01 19:56:22 +0000

    nodemon

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const Article = require('./models/article')
-const articleRouter = require('./routes/articles')
+const Article = require('./Restaurant-CA2/models/article')
+const articleRouter = require('./Restaurant-CA2/routes/articles')
 const methodOverride = require('method-override')
 const app = express()
 
